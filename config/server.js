f9588b6c3043f6a417be90d6e9ecc5bf38833710
Davi Coelho <davi.cmbarreto@gmail.com>
2023-03-26 00:00:58 -0400

    feat: cors instalado;

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(cors({ methods: ['GET'] }))
 app.use('/', express.static('./app/public'))
 
 module.exports = app
